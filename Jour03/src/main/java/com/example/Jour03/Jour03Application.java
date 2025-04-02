16eefe5a9edbee7129e04fec2ece2f6bf2565c8e
baptiste-appriou <baptiste.appriou@laplateforme.io>
2025-04-02 11:07:00 +0200

    Job01 done

---
+++ Jour03/src/main/java/com/example/Jour03/Jour03Application.java
@@ -0,0 +1,13 @@
+package com.example.Jour03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jour03Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jour03Application.class, args);
+	}
+
+}
---
+++ Jour03/src/test/java/com/example/Jour03/Jour03ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Jour03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jour03ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
